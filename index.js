41c585bbcf1cb8667c5c31fe599a91c2bf8e894d
MADALIN-PC\matei <matei.madalin1@gmail.com>
2024-04-23 10:09:48 +0300

    tests

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello Cloud Computing with Azure!');
+  res.send('Hello Cloud Computing with Azure! One update.');
 });
 
 // Start the server
