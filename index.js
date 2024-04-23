85e7b7b6bd8ae61435bc718a142783f2e10072af
MADALIN-PC\matei <matei.madalin1@gmail.com>
2024-04-23 10:14:15 +0300

    Added a new feature. The tests are working.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello Cloud Computing with Azure! One update.');
+  res.send('Hello Cloud Computing with Azure! One update. Lets brake something');
 });
 
 // Start the server
